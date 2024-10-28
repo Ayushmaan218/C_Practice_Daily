d6d6e3c9161cf1b594be2be93abbb98173dd1909
Ayushmaan Mohanty <121386512+Ayushmaan218@users.noreply.github.com>
2024-10-28 20:58:31 +0530

    Add files via upload
    This C program calculates the product of a user-entered integer, starting with an initial product value of 1. The program begins by prompting the user to input an integer. If the input is not 0, the program multiplies the input with the current product (which is initially 1). If the user enters 0, no multiplication is performed, and the product remains 1. This behavior ensures that 0 doesn’t affect the initial product value. The final product is then printed on the screen.
    
    This program demonstrates basic input handling, conditional statements, and simple arithmetic operations in C.

---
+++ Q1.c
@@ -0,0 +1,14 @@
+//Ayushmaan Mohanty
+//2241013230
+
+#include <stdio.h>
+int main(){
+   int item;
+   int p=1;
+   printf("Enter an item");
+   scanf("%d",&item);
+   if(item!=0){
+    p=p*item;
+   }
+   printf("The product is %d",p);
+}
